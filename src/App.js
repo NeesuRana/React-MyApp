2c255624231428fd8df34857c72e8e2b4979c5c8
Neesu Rana <rananisu69@gmail.com>
2024-11-27 12:14:12 +0545

    setup header footer and body in app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 function App() {
-  let name="Neesu";
   return (
     <div className='App'>
-      <h2>Hello World!</h2>
-      <p> {name=='Neesu'? 'Hello Neesu' : 'Hello Guest' } </p>
+      <Header />
+      <Body />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Body.js
@@ -0,0 +1,3 @@
+const Body= () =>{
+    return <div> Body </div>;
+}
