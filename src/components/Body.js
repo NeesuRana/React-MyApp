209f3aaa0a28fc39dd8f54f2040180e715af509d
Neesu Rana <rananisu69@gmail.com>
2024-12-02 12:36:27 +0545

    setup productItem and made it shorter to execute

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,6 +3,51 @@ import ProductCard from './ProductCard';
 import Card from './Card';
 
 const Body = () => {
+  const ProductItems=[{
+"id": 1,
+"title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+"price": 109.95,
+"description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+"category": "men's clothing",
+"image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+"rating": {
+"rate": 3.9,
+"count": 120
+}
+},
+{
+"id": 2,
+"title": "Mens Casual Premium Slim Fit T-Shirts ",
+"price": 22.3,
+"description": "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
+"category": "men's clothing",
+"image": "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+"rating": {
+"rate": 4.1,
+"count": 259
+}
+},
+{
+"id": 4,
+"title": "Mens Cotton Jacket",
+"price": 55.99,
+"description": "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
+"category": "men's clothing",
+"image": "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
+"rating": {
+"rate": 4.7,
+"count": 500
+}
+  }]
+
+  const CardItems=[{
+    "id":0,
+    "title":"Quinn Harley's Blue Diamond Ring",
+    "description" :"Shiny and colored gold rings that makes your finger look cool and elegent.",
+    "price": "$50.77",
+    "image":"https://imgs.search.brave.com/-LoG6WXMwDQFp5xnqVBD3IS9eVQGRbRWszqyq0hc0Yw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzAwLzM3LzU1LzQy/LzM2MF9GXzM3NTU0/MjI4XzFRUW5YVEs5/ZDRmVUhuS1IxalNO/TkwzMnF2YmM1SWl3/LmpwZw"
+  }]
+
   return (
     <section className='flex flex-col gap-4 px-2 py-2 '>
       <div className='flex gap-3'>
@@ -14,55 +59,23 @@ const Body = () => {
         Search
       </div>
       <div className='flex flex-wrap justify-center gap-4 product-items'>
-        <ProductCard 
-            title="Clothing Bag"
-            description="Your perfect looking bag with all the facilities you need within a bag."
-            price="$102.88"
-            image="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
-        />
-        <ProductCard
-         title="Jeans"
-         description="Slim-fitting style, contrast raglan long sleeve, three-button henley placket, 
-         light weight & soft fabric for breathable and comfortable wearing."
-         price= "$22.3"
-         image="https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg"
-        />
+        <ProductCard productItem={ProductItems[0]}/>
+        <ProductCard productItem={ProductItems[1]}/>
+        <ProductCard productItem={ProductItems[2]}/>
+        {/* <ProductCard/>
 
-        <ProductCard
-            title="Mens Cotton Jacket"
-            description="Great outerwear jackets for Spring/Autumn/Winter
-            , suitable for many occasions, such as working, hiking, camping."
-            price= "$555.9"
-            image="https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg"
-        />
-        <ProductCard
-          title="John Hardy Women's Legends Naga Gold & Silver Dragon Station Chain Bracelet"
-          description="From our Legends Collection, the Naga was inspired by the mythical water 
-          dragon that protects the ocean's pearl.Wear facing inward to be bestowed with love and
-           abundance, or outward for protection.."
-          price= "$695"
-          image="https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg"
-        />
+        <ProductCard/>
+        <ProductCard/>
 
-        <ProductCard
-          title="Solid Gold Petite Micropave"
-          description="Satisfaction Guaranteed. Return or exchange any order within 30 days.
-          Designed and sold by Hafeez Center in the United States."
-          price= "$168"
-          image="https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg"
+        <ProductCard/> */}
+        <Card cardItem={CardItems[0]}
         />
-        <Card
-        title="Quinn Harley's Blue Diamond Ring"
-        description="Shiny and colored gold rings that makes your finger look cool and elegent."
-        price= "$50.77"
-            image="https://imgs.search.brave.com/-LoG6WXMwDQFp5xnqVBD3IS9eVQGRbRWszqyq0hc0Yw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzAwLzM3LzU1LzQy/LzM2MF9GXzM3NTU0/MjI4XzFRUW5YVEs5/ZDRmVUhuS1IxalNO/TkwzMnF2YmM1SWl3/LmpwZw"
-        />
-        <Card
+        {/* <Card
             title="Ancient Chinese HairClip"
             description="Clip that secure and adorn women’s hair, often featuring intricate designs, patterns, 
             and materials inspired by Chinese heritage."
             price= "$20"
-            image="https://imgs.search.brave.com/BGTH8Q1GnHZivWibDgjreMijhoiip6E96MdmdWEXyFE/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/bGlsbGFyb3NlLmNv/bS9jZG4vc2hvcC9m/aWxlcy9GWC00Njky/X1RfMV80MTZ4NjAw/LmpwZz92PTE3MDg4/MDU2OTA"
+            image="https://imgs.search.brave.com/Uh7zZ12mb6HneXy24cqvjXPSPftBtHcBK4cM9HEPFec/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/NDFZZXhIcFlJaUwu/anBn"
         />
         <Card
           title="Golden Maria ClawClip"
@@ -74,11 +87,12 @@ const Body = () => {
         />
 
         <Card
-          title="Satin Scrunchies"
-          description="Elevates your style and hair care routine with this luxurious satin scrunchies.
+          title="Satin Scrunchie"
+          description="Elevates your style and hair care routine with this luxurious satin scrunchie.
            Designed for both elegance and comfort, these scrunchies are perfect for all hair types and styles."
           price= "$10.12"
-          image="https://imgs.search.brave.com/d-Z5sWmmlySBtcS0Jk8ljJjHAesIAR69NuQvcW86Fek/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTMx/OTc3MzI3OS9waG90/by9tb29ubG91LXNj/cnVuY2hpZXMuanBn/P3M9NjEyeDYxMiZ3/PTAmaz0yMCZjPUJn/aGNmU2l3bjhPbG5G/dDZHQjdtVS1TWUl4/amZpd1dyZUU3RG15/bndFQTA9"
+          image="https://imgs.search.brave.com/WEgDUs9Az_3QXk2a4iZ6y6rN9oTtx0BGe0bc9KmY0Ck/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/bXlraXRzY2guY29t/L2Nkbi9zaG9wL2Zp/bGVzLzY2MDYyLU1p/Y2tleU1pbm5pZXhL/aXRzY2gtUmVjeWNs/ZWRGYWJyaWNSaGlu/ZXN0b25lU2NydW5j/aGllLTFwYy1DcmVh/bS1wa2ctMTI4MHgx/MjgwcHguanBnP3Y9/MTcyMTg2MTQxNCZ3/aWR0aD03MDA
+          "
         />
 
         <Card
@@ -87,9 +101,9 @@ const Body = () => {
           These airbands come in a variety of materials, including fabric and metal, ensuring 
           there’s a perfect match for your unique taste."
           price= "$10.11"
-          image="https://imgs.search.brave.com/O1SlXyxF0QPsS4IxWE9W9QC1BerY8_ZdKPGggwt4eGE/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pLmV0/c3lzdGF0aWMuY29t/LzE4ODMxODk4L3Iv/aWwvNDYxYmE1LzQ2/NTEwODA0NjEvaWxf/NjAweDYwMC40NjUx/MDgwNDYxX29ubjYu/anBn"
+          image="https://imgs.search.brave.com/Y8hlQ5ZXFP2835D83-_qmJwoa6HSONkQyNwcxJuAgO8/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/anVua2JyYW5kcy5j/b20vY2RuL3Nob3Av/cHJvZHVjdHMvUEVP/TllQQVJUWV9CQkwt/UzIuanBnP3Y9MTU3/MTc3NDA2NyZ3aWR0/aD0xMjgw"
         />
-     
+      */}
       </div>
     </section>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,16 +2,17 @@ import React from 'react';
 
 const Card = (props) => {
   return (
-    <div className='flex flex-col gap-4 px-3 border-2 border-slate-600 w-72'>
+    <div className='flex flex-col gap-4 px-4 py-4 border border-slate-200 w-72 rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300 bg-white'>
       <img
-        src={props.image}
-        alt=''
+        src={props.cardItem.image}
+        alt={props.cardItem.title}
+        className='h-80 object-cover rounded-md hover:scale-105 transition-transform duration-300'
       />
-      <h2 className='text-2xl'>{props.title}</h2>
-      <p>
-        {props.description}
+      <h2 className='text-2xl font-semibold text-gray-800'>{props.cardItem.title}</h2>
+      <p className='text-gray-600 line-clamp-2'>
+        {props.cardItem.description}
       </p>
-      <h3 className='text-2xl'>{props.price}</h3>
+      <h3 className='text-2xl font-bold text-indigo-600' >{props.cardItem.price}</h3>
     </div>
 
     // <div></div>
